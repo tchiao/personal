6751d41449a34fa1554495463c2ae379a6924f68
tchiao <tiffanya.chiao@gmail.com>
2015-02-02 13:30:25 -0800

    Responsive sidebar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,9 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap
+
+$(document).ready(function() {
+  $('[data-toggle=offcanvas]').click(function() {
+    $('.row-offcanvas').toggleClass('active');
+  });
+});
